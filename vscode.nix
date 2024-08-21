411cc83c91bf5aa4dc2a0b0d3423d08f6bb93c9b
SodaVolcano <tin1771718335@gmail.com>
2024-08-21 16:13:19 +0800

    vscode.nix extensions :DDD

--- vscode.nix
+++ vscode.nix
@@ -1,26 +1,28 @@
-{pkgs ? import <nixpkgs> {
-     config.allowUnfree = true;
-}}:
+{
+  pkgs ?
+    import <nixpkgs> {
+      config.allowUnfree = true;
+    },
+}:
 pkgs.mkShell {
   # nativeBuildInputs is usually what you want -- tools you need to run
   nativeBuildInputs = with pkgs.buildPackages; [
     vscode
     (vscode-with-extensions.override {
-      vscodeExtensions = with vscode-extensions;
-        [
-          bbenoist.nix
-          ms-python.python
-          ms-azuretools.vscode-docker
-          ms-vscode-remote.remote-ssh
-        ]
-        ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
-          {
-            name = "remote-ssh-edit";
-            publisher = "ms-vscode-remote";
-            version = "0.47.2";
-            sha256 = "1hp6gjh4xp2m1xlm1jsdzxw9d8frkiidhph6nvl24d0h8z34w49g";
-          }
-        ];
+      vscodeExtensions = with vscode-extensions; [
+        bbenoist.nix
+        ms-python.vscode-pylance
+        ms-python.python
+        ms-toolsai.jupyter
+        ms-toolsai.jupyter-keymap
+        ms-python.debugpy
+        ms-python.vscode-pylance
+        ms-python.black-formatter
+        vscodevim.vim # <33333
+        christian-kohler.path-intellisense
+        visualstudioexptteam.vscodeintellicode
+        usernamehw.errorlens # inline error message
+      ];
     })
   ];
 }
