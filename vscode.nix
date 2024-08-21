0b957316fbabc88ec3504beeb1357e1122b11be2
SodaVolcano <tin1771718335@gmail.com>
2024-08-21 16:10:22 +0800

    vscode.nix allow unfree

--- vscode.nix
+++ vscode.nix
@@ -1,4 +1,6 @@
-{pkgs ? import <nixpkgs> {}}:
+{pkgs ? import <nixpkgs> {
+     config.allowUnfree = true;
+}}:
 pkgs.mkShell {
   # nativeBuildInputs is usually what you want -- tools you need to run
   nativeBuildInputs = with pkgs.buildPackages; [
