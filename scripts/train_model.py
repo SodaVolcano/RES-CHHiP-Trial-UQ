f442272144158efb118dc74f917337919dc99dd9
SodaVolcano <tin1771718335@gmail.com>
2024-12-11 13:04:08 +0800

    refactored model training script and optional arguments

--- scripts/train_model.py
+++ scripts/train_model.py
@@ -1,66 +1,107 @@
-import os
+"""
+Train model(s) on a dataset h5 files.
+
+usage - training a single unet model on fold 0:
+    uv run python3 path/to/train_model.py --fold 0 unet
+usage - training 3 unet models on fold 2:
+    uv run python3 path/to/train_model.py --fold 2 unet_3
+usage - training 3 unet models and 1 confidnet on all folds:
+    uv run python3 path/to/train_model.py unet_3 confidnet
+"""
+
+from pathlib import Path
 import sys
 
 import torch
-from lightning import Trainer
-from lightning.pytorch.callbacks import ModelCheckpoint
 from loguru import logger
 
-from uncertainty.training.datasets import H5Dataset, SegmentationData
-from uncertainty.training.training import (
+from __helpful_parser import HelpfulParser
+
+sys.path.append("..")
+sys.path.append(".")
+
+from uncertainty import configuration
+from uncertainty.utils import config_logger
+from uncertainty.training import (
+    H5Dataset,
+    SegmentationData,
     init_training_dir,
     read_training_fold_file,
     train_models,
 )
 
-sys.path.append("..")
-sys.path.append(".")
-import dill
-import torch
-from __helpful_parser import HelpfulParser
-from lightning.pytorch.loggers import TensorBoardLogger
 
-from uncertainty import configuration
-from uncertainty.utils import config_logger
-
-
-def main(
-    config: dict, h5_path: str, train_dir: str, config_path: str, models: list[str]
+def train_one_fold(
+    fold_idx: int,
+    h5_path: str,
+    config: dict,
+    models: list[str],
+    fold_split_path: str | Path,
+    ckpt_path: str | Path,
 ):
+    train_indices, val_indices, seed = read_training_fold_file(
+        fold_split_path, fold_idx
+    )
+    dataset = SegmentationData(
+        h5_path,
+        train_indices=train_indices,  # type: ignore
+        val_indices=val_indices,  # type: ignore
+        **config,
+    )
+    train_models(
+        models,
+        dataset=dataset,
+        checkpoint_dir=ckpt_path,
+        experiment_name=f"fold_{fold_idx}",
+        seed=seed,  # type: ignore
+        **config,
+    )
+
+
+def setup(h5_path: str, train_dir: str, config_path: str, config: dict):
     torch.set_float32_matmul_precision("medium")
     torch.autograd.set_detect_anomaly(True)
 
     # split into test set
     dataset = H5Dataset(h5_path)
-
     res = init_training_dir(train_dir, config_path, dataset.indices, **config)
+
     assert (
         res != None
     ), f"Failed to initialise training directory, specified configuration file at {config_path} already exist in training directory!"
     _, fold_split_path, checkpoint_paths = res
+    return fold_split_path, checkpoint_paths
+
+
+def train_models_for_one_fold(
+    config: dict,
+    config_path: str,
+    h5_path: str,
+    train_dir: str,
+    fold_idx: int,
+    models: list[str],
+):
+    fold_split_path, checkpoint_paths = setup(h5_path, train_dir, config_path, config)
+    train_one_fold(
+        fold_idx, h5_path, config, models, fold_split_path, checkpoint_paths[fold_idx]
+    )
+
+
+def train_models_for_all_folds(
+    config: dict,
+    config_path: str,
+    h5_path: str,
+    train_dir: str,
+    models: list[str],
+):
+    fold_split_path, checkpoint_paths = setup(h5_path, train_dir, config_path, config)
 
     for fold_idx, ckpt_path in enumerate(checkpoint_paths):
-        train_indices, val_indices, seed = read_training_fold_file(
-            fold_split_path, fold_idx
-        )
-        dataset = SegmentationData(
-            h5_path,
-            train_indices=train_indices,  # type: ignore
-            val_indices=val_indices,  # type: ignore
-            **config,
-        )
-        train_models(
-            models,
-            dataset=dataset,
-            checkpoint_dir=ckpt_path,
-            experiment_name=f"fold_{fold_idx}",
-            seed=seed,  # type: ignore
-            **config,
-        )
+        train_one_fold(fold_idx, h5_path, config, models, fold_split_path, ckpt_path)
 
 
 if __name__ == "__main__":
-    parser = HelpfulParser(description="Train a model on a dataset h5 files.")
+    parser = HelpfulParser(description="Train model(s) on a dataset h5 files.")
     parser.add_argument(
         "--config",
         "-c",
@@ -73,14 +114,14 @@ if __name__ == "__main__":
         "-i",
         type=str,
         help="Path to the H5 dataset of PatientScan dictionaries. If not provided, the h5_path from the configuration file will be used.",
-        optional=True,
+        required=False,
     )
     parser.add_argument(
         "--train-dir",
         "-o",
         type=str,
         help="Model training directory to save or load model checkpoints. A folder container folders for each fold will be created, and the checkpoints for each model will be saved in the respective fold folder. If not provided, the checkpoint_dir from the configuration file will be used.",
-        optional=True,
+        required=False,
     )
     parser.add_argument(
         "--logging",
@@ -90,11 +131,17 @@ if __name__ == "__main__":
         default=True,
     )
     parser.add_argument(
-        "--models",
-        "-m",
+        "models",
         type=str,
-        help="List of models to train, separated by comma.",
-        default="",
+        nargs="+",
+        help="List of model names to train. To train multiple models with the same name, specify quantity as '<model_name>_<int>. e.g. 'unet_3' will train 3 UNet models.",
+    )
+    parser.add_argument(
+        "--fold",
+        "-f",
+        type=int,
+        help="Fold number to train the model on. If not specified, all folds will be trained.",
+        required=False,
     )
 
     args = parser.parse_args()
@@ -104,11 +151,20 @@ if __name__ == "__main__":
         logger.enable("uncertainty")
         config_logger(**config)
 
-    # TODO: add retrain argument
-    main(
-        config=config,
-        config_path=args.config,
-        h5_path=args.in_path or config["data__h5_path"],
-        train_dir=args.train_dir or config["training__train_dir"],
-        models=args.models.split(",") if args.models else config["training__models"],
-    )
+    if args.fold is not None:
+        train_models_for_one_fold(
+            config,
+            args.config,
+            args.in_path or config["data__h5_path"],
+            args.train_dir or config["training__train_dir"],
+            args.fold,
+            args.models,
+        )
+    else:
+        train_models_for_all_folds(
+            config,
+            args.config,
+            args.in_path or config["data__h5_path"],
+            args.train_dir or config["training__train_dir"],
+            args.models,
+        )
